ce1e145c88909076bd8e9a437e1b0d8373e19068
vipulll <vjvipuljain3@gmail.com>
2023-09-24 13:47:43 +0530

    Edit and delete

--- BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -2,29 +2,37 @@ using BulkyWebRazor_Temp.Data;
 using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Identity.Client;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
     public class EditModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
 
-        //private readonly ApplicationDbContext _db;
-        //public Category CategoriesList { get; set; }
-        //public EditModel(ApplicationDbContext db)
-        //{
-        //    _db = db;
-        //}
-        public void OnGet()
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
         }
 
-        //public IActionResult OnPost()
-        //{
-        //    //_db.Categories.Update();
-        //    _db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
     }
 }
